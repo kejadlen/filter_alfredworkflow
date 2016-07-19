d9774dd0c11c5c334ce745f516b55902bf07509b
Alpha Chen <alpha.chen@gmail.com>
2016-07-18 21:35:49 -0700

    omg

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,55 @@
 extern crate plist;
 
-use plist::Plist;
-use std::io::{self, Read};
+use plist::{self, Plist};
+use std::collections::btree_map::BTreeMap;
+use std::io::{self, Read, Write};
 
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
     let cursor = io::Cursor::new(buffer);
-    let plist = Plist::read(cursor).unwrap();
+    let mut plist = Plist::read(cursor).unwrap();
 
-    let dict = match plist {
-                   Plist::Dictionary(dict) => Some(dict),
-                   _ => None,
-               }
-               .unwrap();
-    let vars = match dict.get("variablesdontexport") {
-                   Some(&Plist::Array(ref array)) => Some(array),
-                   _ => None,
-               }
-               .unwrap();
+    let mut dict = match plist {
+                       Plist::Dictionary(ref mut dict) => Some(dict),
+                       _ => None,
+                   }
+                   .unwrap();
+    let vars = variables_dont_export(&dict);
 
-    println!("{:?}", vars);
+    let mut variables = match dict.get_mut("variables") {
+                            Some(&mut Plist::Dictionary(ref mut dict)) => {
+                                Some(dict)
+                            }
+                            _ => None,
+                        }
+                        .unwrap();
+    for var in vars {
+        variables.insert(var.clone(), Plist::String("".into()));
+    }
+
+    // println!("{:?}", dict);
+    println!("{:?}", variables);
+
+    // io::stdout().write(b"hello world").unwrap();
+    let serializer = plist::Serializer::new(io::stdout());
+    serializer.write(dict);
+}
+
+fn variables_dont_export(dict: &BTreeMap<String, Plist>) -> Vec<String> {
+    match dict.get("variablesdontexport") {
+        Some(&Plist::Array(ref array)) => Some(array),
+        _ => None,
+    }
+    .unwrap()
+    .iter()
+    .map(|x| {
+        match x {
+            &Plist::String(ref string) => Some(string),
+            _ => None,
+        }
+        .unwrap()
+        .clone()
+    })
+    .collect::<Vec<_>>()
 }
