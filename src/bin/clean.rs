24b0ff48833ebfe20a56bd222c06b657b9c5ede6
Alpha Chen <alpha@kejadlen.dev>
2023-03-27 12:24:47 -0700

    Better errors

--- src/bin/clean.rs
+++ src/bin/clean.rs
@@ -1,29 +1,38 @@
-extern crate plist;
-
+use color_eyre::eyre::{anyhow, Result};
 use plist::Value;
 use std::io::{self, Read};
 
-fn main() {
+fn main() -> Result<()> {
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).unwrap();
+    io::stdin().read_to_string(&mut buffer)?;
     let cursor = io::Cursor::new(buffer);
-    let mut plist = Value::from_reader(cursor).unwrap();
+    let mut plist = Value::from_reader(cursor)?;
 
-    clean_vars(&mut plist);
+    clean_vars(&mut plist)?;
 
-    plist.to_writer_xml(io::stdout()).unwrap();
+    plist.to_writer_xml(io::stdout())?;
+
+    Ok(())
 }
 
-fn clean_vars(plist: &mut Value) {
-    let dict = plist.as_dictionary_mut().unwrap();
+fn clean_vars(plist: &mut Value) -> Result<()> {
+    let dict = plist
+        .as_dictionary_mut()
+        .ok_or_else(|| anyhow!("error opening plist"))?;
     let vars = variables_dont_export(dict);
+    if vars.is_empty() {
+        return Ok(());
+    }
 
-    let variables = dict.get_mut("variables")
-                            .and_then(|x| x.as_dictionary_mut())
-                            .unwrap();
+    let variables = dict
+        .get_mut("variables")
+        .and_then(|x| x.as_dictionary_mut())
+        .ok_or_else(|| anyhow!(""))?;
     for var in vars {
         variables.insert(var.clone(), Value::String("".into()));
     }
+
+    Ok(())
 }
 
 fn variables_dont_export(dict: &plist::Dictionary) -> Vec<String> {
@@ -31,8 +40,8 @@ fn variables_dont_export(dict: &plist::Dictionary) -> Vec<String> {
         .and_then(|x| x.as_array())
         .map(|x| {
             x.iter()
-             .map(|x| x.as_string().unwrap().into())
-             .collect::<Vec<_>>()
+                .map(|x| x.as_string().unwrap().into())
+                .collect::<Vec<_>>()
         })
         .unwrap_or_else(Vec::new)
 }
